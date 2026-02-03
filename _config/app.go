7af4b03d1d4df799c9a36f800b7cf911a60a5ff8
Sirawit Pratoomsuwan <sirawit.p@connectedtech.co.th>
2026-02-03 19:14:05 +0700

    refactor: remove app config unused

--- _config/app.go
+++
@@ -1,6 +0,0 @@
-package config
-
-type AppConfig struct {
-	Debug    bool   `mapstructure:"debug" yaml:"debug"`
-	LogLevel string `mapstructure:"log_level" yaml:"log_level"`
-}
--- _config/config.go
+++ _config/config.go
@@ -5,7 +5,6 @@ import "go.uber.org/fx"
 type Config struct {
 	fx.Out `yaml:"-"`
 
-	App AppConfig `mapstructure:"app" yaml:"app"`
 	Jwt JwtConfig `mapstructure:"jwt" yaml:"jwt"`
 	Db  DbConfig  `mapstructure:"db" yaml:"db"`
 }
