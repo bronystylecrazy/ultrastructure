067e7aa9665301b00afceab7096c1ed12775e4be
Sirawit Pratoomsuwan <sirawit.p@connectedtech.co.th>
2026-02-09 15:41:36 +0700

    refactor: remove unused appender

--- storage/s3/provider.go
+++ storage/s3/provider.go
@@ -1,8 +1,6 @@
 package s3
 
 import (
-	"fmt"
-
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/smithy-go/middleware"
 )
@@ -12,9 +10,6 @@ type Appender interface {
 }
 
 func NewS3Client(cfg aws.Config, s3cfg Config, appender ...Appender) *Client {
-
-	fmt.Println("appenders:", len(appender))
-
 	for _, a := range appender {
 		a.Append(&cfg.APIOptions)
 	}
